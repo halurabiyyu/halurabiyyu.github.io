a05207589b516d6f9e30e067e2f68e73e47856d5
halurabiyyu <halurabiyyu@gmail.com>
2023-10-20 12:05:10 +0700

    Update : Navbar Menu

--- js/script.js
+++ js/script.js
@@ -15,6 +15,18 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.padding = "30px 10px";
+    document.getElementById("head").style.fontSize = "25px";
+  } else {
+    document.getElementById("header").style.padding = "80px 10px";
+    document.getElementById("head").style.fontSize = "35px";
+  }
+}
+
 function addBook() {
     const textBook = document.getElementById('title').value;
     const pageBook = document.getElementById('page').value;
